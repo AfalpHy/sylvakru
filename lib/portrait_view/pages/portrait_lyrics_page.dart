0a81849790c4073cad0c3b49448ccf97010402ec
AfalpHy <736353503@qq.com>
2026-09-19 12:34:52 +0800

    trivial

--- lib/portrait_view/pages/portrait_lyrics_page.dart
+++ lib/portrait_view/pages/portrait_lyrics_page.dart
@@ -299,6 +299,7 @@ class _PortraitLyricsPageState extends State<PortraitLyricsPage> {
             picture: currentSong?.picture,
             elevation: 15,
             color: colorManager.getSpecificLyricsPageCoverArtBaseColor(),
+            useResize: false,
           ),
         ),
 
