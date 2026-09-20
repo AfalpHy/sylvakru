48b0d26fac6fceef50509e6a0e8d24ba306e07da
AfalpHy <736353503@qq.com>
2026-09-20 10:32:26 +0800

    trivial

--- lib/base/widgets/my_scaffold.dart
+++ lib/base/widgets/my_scaffold.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:sylvakru/base/app.dart';
 import 'package:sylvakru/portrait_view/custom_appbar_leading.dart';
@@ -10,22 +9,20 @@ Widget myScaffold({
   String? title,
   List<Widget>? actions,
 }) {
-  return AnnotatedRegion<SystemUiOverlayStyle>(
-    value: mainPageThemeNotifier.value == .dark ? .light : .dark,
-    child: Scaffold(
+  return Scaffold(
+    backgroundColor: Colors.transparent,
+    resizeToAvoidBottomInset: false,
+    appBar: AppBar(
+      automaticallyImplyLeading: false,
+      leading: customAppBarLeading(context, label: label),
       backgroundColor: Colors.transparent,
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: customAppBarLeading(context, label: label),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: title != null ? Text(title, style: .new()) : null,
-        scrolledUnderElevation: 0,
-        centerTitle: true,
-        actions: actions,
-      ),
-      body: body,
+      systemOverlayStyle: mainPageThemeNotifier.value == .dark ? .light : .dark,
+      elevation: 0,
+      title: title != null ? Text(title, style: .new()) : null,
+      scrolledUnderElevation: 0,
+      centerTitle: true,
+      actions: actions,
     ),
+    body: body,
   );
 }
